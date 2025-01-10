215c6415be1b522725420c82fb43dd0398084240
Shishir Shrestha <shresthashishir91@gmail.com>
2025-01-10 21:25:38 +0545

    demo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,14 +10,13 @@ import Login from "./pages/Login";
 import Rewards from "./pages/Rewards";
 const App = () => {
   return (
-    <LoyalityCard />
-    // <Router>
-    //   <Routes>
-    //     <Route index element={<Login />} />
-    //     <Route path="/loyality" element={<LoyalityCard />} />
-    //     <Route path="/rewards" element={<Rewards />} />
-    //   </Routes>
-    // </Router>
+    <Router>
+      <Routes>
+        <Route index element={<Login />} />
+        <Route path="/loyality" element={<LoyalityCard />} />
+        <Route path="/rewards" element={<Rewards />} />
+      </Routes>
+    </Router>
   );
 };
 
